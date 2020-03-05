d0fc09d19e9eaed9d230386d855d7cb635fdf3d1
PavelBelka <PavelBelka@users.noreply.github.com>
2020-03-05 12:58:51 +0400

    обновление 2

--- Прошивка/main.c
+++ Прошивка/main.c
@@ -12,18 +12,20 @@
 
 volatile uint8_t data_mcp[2] = {0, 0};
 volatile uint8_t data_max[4] = {0, 0, 0, 0};
-volatile uint8_t avaliable_spi = 0, transmit_spi = 0, step_transmit = 0, counter_recive = 0;
-volatile uint16_t counter_update_max = 0;
-uint8_t mode = 1; // режим работы
+volatile uint8_t data_recive[3] = {0, 0, 0};
+volatile uint8_t data_transmit[3] = {0, 0, 0};
+volatile uint8_t avaliable_spi = 0, avaliable_usart = 0, transmit_usart = 0, transmit_spi = 0, step_transmit = 0, counter_recive = 0, counter_usart = 0, connect = 0;
+volatile uint16_t counter_update_max = 0, usart_recive_data = 0;
+volatile uint8_t mode = 0; // режим работы
 
 void initialization()
 {
 	// Настройка порта B: PB0 - вход, PB1 - выход, (SCK, MOSI) - выход, MISO - вход
 	DDRB |= (1 << PORTB1) | (1 << SPI_sck) | (1 << SPI_mosi);
 	DDRB &= ~((1 << PORTB0) | (1 << SPI_miso));
-	// Настройка порта D: PD7 - вход, PD6 - выход, PD5 - выход
-	DDRD |= (1 << PORTD6) | (1 << PORTD5);
-	DDRD &= ~(1 << PORTD7);
+	// Настройка порта D: PD7, PD0 - вход, PD6, PD5, PD1 - выход
+	DDRD |= (1 << PORTD6) | (1 << PORTD5) | (1 << PORTD1);
+	DDRD &= ~((1 << PORTD7) | (1 << PORTD0));
 	// Настройка порта C: PC0 - выход, PC1 - выход
 	DDRC |= (1 << CS_max31855) | (1 << CS_mcp41010);
 	PORTB &= ~((1 << SPI_sck) | (1 << SPI_mosi));
@@ -34,7 +36,7 @@ void initialization()
 	// Настройка USART: асинхронный режим, 8 бит посылка, 1 стоп-бит, контроль четности отключен, скорость 9600 бод, прерывание по приему
 	UCSR0A |= (1 << U2X0); //включаем ускоритель
 	UBRR0 = 207;
-	UCSR0B |= (1 << TXEN0);
+	UCSR0B |= (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0) | (1 << TXCIE0);
 	UCSR0C |= (1 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00);
 	UCSR0C &= ~((1 << UMSEL00) | (1 << UMSEL01) | (1 << USBS0));
 	// Настройка таймера 0: предделитель на 1024, прерывание по переполнению включен
@@ -43,12 +45,12 @@ void initialization()
 	sei();
 }
 
-void spi_treansmit_mcp(uint8_t command, uint8_t data)//отправка по spi на mcp41010
+void spi_transmit_mcp(uint8_t command, uint8_t data)//отправка по spi на mcp41010
 {
 	avaliable_spi = 1; //флаг занятости шины spi
-	PORTC &= ~(1 << CS_mcp41010); //прижимаем CS к земле
 	data_mcp[0] = command;
 	data_mcp[1] = data;
+	PORTC &= ~(1 << CS_mcp41010); //прижимаем CS к земле
 	transmit_spi = 1; // сообщаем, что занимаемся отправкой
 	SPDR = data_mcp[0]; // отправляем 1 пакет данных
 }
@@ -60,14 +62,22 @@ void spi_reception_max31855()
 	SPDR = 0xFF;
 }
 
-void USART_Transmit(uint8_t *data)
+void USART_Transmit(uint8_t command, uint16_t data) // передача команды и данных по uart
 {
-	while(*data != '\0')
-	{
-		while( !(UCSR0A & (1 << UDRE0))); //ожидаем опустошения буфера приема
-		UDR0 = *data; //записываем данные
-		data++;
-	}
+	//делим на пакеты
+	data_transmit[0] = command;
+	data_transmit[1] = data >> 8;
+	data_transmit[2] = data & 0xFF;
+	avaliable_usart = 1; // говорим что занят uart
+	transmit_usart = 1;
+	counter_usart = 0;
+	UDR0 = data_transmit[0]; //записываем данные
+}
+
+volatile uint16_t temperature_avaliable()
+{
+	uint16_t temp = (data_max[0] << 4) | (data_max[1] >>4);
+	return temp;
 }
 
 ISR(SPI_STC_vect)
@@ -90,13 +100,18 @@ ISR(SPI_STC_vect)
 	}
 	else
 	{
-		data_max[counter_recive] = SPDR;
-		counter_recive++;
-		if (counter_recive > 3)
+		data_max[step_transmit] = SPDR;
+		step_transmit++;
+		if (step_transmit > 3)
 		{
 			avaliable_spi = 0;
 			PORTC |= (1 << CS_max31855);
-			counter_recive = 0;
+			step_transmit = 0;
+			if ((data_max[1] & 0b00000001) == 0b00000001)
+			{
+				mode = 3;
+				USART_Transmit(12,0);
+			}
 		}
 		else
 		{
@@ -108,12 +123,59 @@ ISR(SPI_STC_vect)
 ISR(TIMER0_OVF_vect)
 {
 	counter_update_max++;
-	if (counter_update_max == 61)
+	if (counter_update_max == 10)
 	{
 		if (avaliable_spi == 0)
 		{
 			counter_update_max = 0;
 			spi_reception_max31855();
+		}
+	}
+}
+
+ISR(USART_TX_vect)
+{
+	if (transmit_usart == 1)
+	{
+			UDR0 = data_transmit[counter_usart + 1];// посылаем следующий пакет
+			counter_usart++;
+			if (counter_usart > 2)//последняя посылка
+			{
+				counter_usart = 0;
+				avaliable_usart = 0;
+				transmit_usart = 0;
+			}
+	}
+}
+
+ISR(USART_RX_vect)
+{
+	data_recive[counter_recive] = UDR0;
+	counter_recive ++;
+	if (counter_recive > 2) // если приняли все пакеты
+	{
+		counter_recive = 0;
+		switch(data_recive[0]) //смотрим, что за команда пришла
+		{
+			case 1:
+				mode = data_recive[2];
+				break;
+			case 2:
+				USART_Transmit(13, temperature_avaliable());
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:// компьютер принял пакет
+				break;
+			case 6:
+				if ((data_recive[1] == 195) && (data_recive[2] == 204)) // запрос от "своей" программы
+				{
+					connect = 1;
+					USART_Transmit(0x10,0x3C33);
+				}
+				break;
 		}
 	}
 }
@@ -131,14 +193,21 @@ int main(void)
 				// отключаем инвертор
 				if (avaliable_spi == 0)
 				{
-					spi_treansmit_mcp(0b00010001,0);
-					break;
+					spi_transmit_mcp(0b00010001,0);
 				}
 				break;
 			case 1: //режим подготовки к измерению
+				if ((avaliable_usart == 0) && (connect == 1))
+				{
+					USART_Transmit(13, temperature_avaliable());
+				}
 				break;
 			case 2: //режим измерения
 				
+				break;
+			case 3:
+				PORTD &= ~(1 << Led_green);
+				spi_transmit_mcp(0b00010001,0);
 				break;
 		}
     }
